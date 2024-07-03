66a21be505d2c51968a66922455178ffd9c5e90d
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-04 01:04:57 +0530

    Add project files.

---
+++ CRUD_Using_EntityFramework/Models/StudentPartial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Using_EntityFramework.Models
+{
+
+    [MetadataType(typeof(studentMetaData))]
+    public partial class student
+    {
+
+       }
+
+    public class studentMetaData
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public Nullable<System.DateTime> DOB { get; set; }
+
+
+    }
+}
