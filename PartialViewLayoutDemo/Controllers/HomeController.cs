520b123e4af4e7ea89566eb6443bd06291243cd5
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-05 19:38:26 +0530

    partialview

---
+++ PartialViewLayoutDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewLayoutDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Index1()
+        {
+            return View();
+        }
+        public PartialViewResult PartialViewResult()
+        {
+            return PartialView("_partialview");
+        }
+        public PartialViewResult PartialViewResult1()
+        {
+            return PartialView("_partialview");
+        }
+    }
+}
