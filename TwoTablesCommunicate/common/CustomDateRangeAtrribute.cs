a837ad0f0cb252679fcc7709de5e33f05c73d6d3
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-07 12:12:09 +0530

    Apply all validation in mvc

---
+++ TwoTablesCommunicate/common/CustomDateRangeAtrribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TwoTablesCommunicate.common
+{
+    public class CustomDateRangeAtrribute:RangeAttribute
+    {
+      public  CustomDateRangeAtrribute() : base(typeof(DateTime),DateTime.Now.ToString(),DateTime.Now.ToString()) { }
+
+        public override bool IsValid(object value)
+        {
+           
+            DateTime inputtime = (DateTime)value;
+            if (inputtime < DateTime.Now)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        
+           
+        }
+
+    }
+}
