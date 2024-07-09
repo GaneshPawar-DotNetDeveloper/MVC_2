7a370662e00141f877da14b751d1ca4139b9da9d
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-10 01:00:02 +0530

    Ajax,Areas,bundling

---
+++ AreasDemo/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AreasDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult partialView1()
+        {
+            return PartialView("_PartialView1");
+        }
+        public PartialViewResult partialView2()
+        {
+            return PartialView("_PartialView2");
+        } 
+        public PartialViewResult partialView3()
+        {
+            return PartialView("_PartialView3");
+        }
+    }
+}
